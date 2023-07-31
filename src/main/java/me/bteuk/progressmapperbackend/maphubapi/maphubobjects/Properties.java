3a9b0fc7e96669e1592c4a4d4293180129903bde
George <georgewalk168@gmail.com>
2023-07-31 18:21:55 +0100

    Added stroke color for LineStrings, media url and images to the Properties of features. Added a constructor for Properties.

--- src/main/java/me/bteuk/progressmapperbackend/maphubapi/maphubobjects/Properties.java
+++ src/main/java/me/bteuk/progressmapperbackend/maphubapi/maphubobjects/Properties.java
@@ -3,9 +3,13 @@ package me.bteuk.progressmapperbackend.maphubapi.maphubobjects;
 public class Properties
 {
     public String fill;
+    public String stroke;
+    public String media_url;
     private long group;
     public String title;
     public String description;
+
+    private Image image;
 
     public void appendTestTextToDescription()
     {
@@ -24,5 +28,21 @@ public class Properties
         }
         else
             this.title = title +" plus the edit from the program !";
+    }
+
+    public Properties(long group, String fill, String title, String description)
+    {
+        this.group = group;
+        this.fill = fill;
+        this.title = title;
+        this.description = description;
+    }
+
+    public Properties(String fill, String title, String description)
+    {
+        this.group = 0;
+        this.fill = fill;
+        this.title = title;
+        this.description = description;
     }
 }
