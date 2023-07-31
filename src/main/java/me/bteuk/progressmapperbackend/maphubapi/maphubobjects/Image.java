36a273bc6e913118a8f2506cedb24ca09a084009
George <georgewalk168@gmail.com>
2023-07-31 18:17:57 +0100

    Added image class to store the information of images on Features of the map

---
+++ src/main/java/me/bteuk/progressmapperbackend/maphubapi/maphubobjects/Image.java
@@ -0,0 +1,11 @@
+package me.bteuk.progressmapperbackend.maphubapi.maphubobjects;
+
+public class Image
+{
+    String id;
+    int w;
+    int h;
+    String tip_color;
+    String avg_color;
+    String caption;
+}
