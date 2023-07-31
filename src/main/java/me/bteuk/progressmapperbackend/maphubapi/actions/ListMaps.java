9f6e9aa4e5ac93548a579ff87573fc454d4161be
George <georgewalk168@gmail.com>
2023-07-31 10:49:51 +0100

    Adds handling of 'list maps' requests.

---
+++ src/main/java/me/bteuk/progressmapperbackend/maphubapi/actions/ListMaps.java
@@ -0,0 +1,27 @@
+package me.bteuk.progressmapperbackend.maphubapi.actions;
+
+import com.google.gson.Gson;
+import me.bteuk.progressmapperbackend.maphubapi.MapHubAPIHandler;
+import me.bteuk.progressmapperbackend.maphubapi.MapHubAPIResponse;
+
+public class ListMaps
+{
+    private static final String szEndpointURL = "https://maphub.net/api/1/map/list";
+
+    private static ListMaps listMaps(String szApiKey)
+    {
+        Gson gson = new Gson();
+        String jsonResponse = listMapsRawResponse(szApiKey);
+
+        //ListMaps currently does not have any objects defined in it so this will not produce any data in maps
+        ListMaps maps = gson.fromJson(jsonResponse, ListMaps.class);
+        return maps;
+    }
+
+    public static String listMapsRawResponse(String szApiKey)
+    {
+        MapHubAPIResponse listMapsResponse = MapHubAPIHandler.request(szApiKey, szEndpointURL, "");
+        assert listMapsResponse != null;
+        return listMapsResponse.getStringContent();
+    }
+}
